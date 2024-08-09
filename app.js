6423ceb57080d812921024b294d4837cbd1de830
S/G___Agent <sajjanchaudhary257@gmail.com>
2024-08-09 11:13:58 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+a=parseInt(prompt("Enter the first number"))
+b=parseInt(prompt("Enter the first number"))
+result=a+b;
+result1=a-b;
+result2=a*b;
+result3=a/b;
+alert("result is " +result)
+alert("result is " +result1)
+alert("result is " +result2)
+alert("result is " +result3)
